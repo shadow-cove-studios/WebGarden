e4f849f822f8f8850ed682e18c7db762738e1b2d
HopeJ-Personal <HopeJ-Personal@gmail.com>
2026-07-27 14:17:27 -0700

    ButtonDemonstration

--- init.js
+++ init.js
@@ -1,1 +1,38 @@
 console.log("test")
+
+var clickOrder = []
+
+function clicka(bid) {
+    console.log("Button Clicked")
+
+    if (clickOrder.length == 2) {
+        clickOrder = []
+    }
+
+    clickOrder.push(bid)
+    checkClick()
+}
+
+function checkClick() {
+    console.log(clickOrder.length)
+    console.log(clickOrder.length == 2)
+    console.log(clickOrder[0] == 1)
+    console.log(clickOrder[1] == 2)
+    if (
+        clickOrder.length == 2 &&
+        clickOrder[0] == 1 &&
+        clickOrder[1] == 2
+    ) {
+        var element = document.querySelector('.Result')
+        element.style.backgroundColor = 'yellow';
+        console.log("Passed")
+    } else if (
+        clickOrder.length == 2 &&
+        clickOrder[0] != 1 ||
+        clickOrder[1] != 2
+    ) {
+        var element = document.querySelector('.Result')
+        element.style.backgroundColor = 'red';
+        console.log("Failed")
+    }
+}
