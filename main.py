0d20f35c8dd74538580f9f6fdb4acdeb5c4e8217
HopeJ-Personal <HopeJ-Personal@gmail.com>
2026-07-28 21:18:42 -0700

    Changed color to test change reflects to build

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def main():
             if event.type == pygame.QUIT:
                 running = False
                 
-        screen.fill((0, 0, 0))
+        screen.fill((0, 0, 20))
         pygame.display.flip()
         
         # CRITICAL: Gives control back to the browser thread
