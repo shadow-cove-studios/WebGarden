73f4801a268b35cf85f09534aed5314a4aca1919
HopeJ-Personal <HopeJ-Personal@gmail.com>
2026-07-28 21:58:12 -0700

    Update main to silence tick and update html groups

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ async def main():
                 running = False
         
         screen.fill((30, 30, 30)) # Dark gray background
-        print("Tick")
+        #print("Tick")
         pygame.display.flip()
         
+        # CRITICAL: This allows the browser to render frames without crashing
         await asyncio.sleep(0) 
         clock.tick(60)
 
+# Yes this normally would be an error however since pyscript runs the python file in an async top level awaits are permitted. ik its weird
 await main()
